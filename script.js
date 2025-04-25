f3a707ee40e3e1e64614f67a8d2b2c7f1631dfdf
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-04-25 16:18:30 -0400

    feat: Add prototype for display function

---
+++ script.js
@@ -0,0 +1,7 @@
+function updateDisplay() {
+    /* Updates the calculator display
+    to reflect the users button selections. */
+
+    // Calculator display window
+    const displayWindow = document.getElementById('display-window');
+}
