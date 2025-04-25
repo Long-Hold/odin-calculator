42124159e261960138cde6b9d8963f090ffaf431
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-04-25 16:28:21 -0400

    feat: Add calculate() function to evaluate expressions

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+function calculate(operand, digit1, digit2) {
+    switch (operand) {
+        case '+':
+            return digit1 + digit2;
+        case '-':
+            return digit1 - digit2;
+        case '*':
+            return digit1 * digit2;
+        case '/':
+            return digit1 / digit2;
+    };
+}
+
 function updateDisplay() {
     /* Updates the calculator display
     to reflect the users button selections. */
