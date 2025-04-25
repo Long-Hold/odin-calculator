4d96c8998577de47200cd9cc5dfd5c49b9bcb7bf
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-04-25 01:18:53 -0400

    feat: Add setters and getters

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 class Calculator {
+    #firstDigit;
+    #operand;
+    #secondDigit;
+
     constructor(firstDigit, operand, secondDigit) {
+        // These call the setters, not the variables passed directly
         this.firstDigit = firstDigit;
         this.operand = operand;
         this.secondDigit = secondDigit;
+    }
+
+    get firstDigit() {
+        return this.#firstDigit;
+    }
+
+    set firstDigit(value) {
+        if (typeof value !== 'number' || isNaN(value)) {
+            throw new Error('First digit must be a valid number');
+        }
+        this.#firstDigit = value;
+    }
+
+    get operand() {
+        return this.#operand;
+    }
+
+    set operand(symbol) {
+        
     }
 
     // Private method that returns a calculated solution
