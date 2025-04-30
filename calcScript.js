3b7e98746c7e642a270858724c836ae16b8f6496
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-04-30 13:58:44 -0400

    refactor: Change error message format

--- calcScript.js
+++ calcScript.js
@@ -4,9 +4,15 @@ class Calculator {
     #rightValue;
 
     constructor(leftValue = null, operand = null, rightValue = null) {
+        this.validOperands = ['+', '-', '*', '/'];
+
         this.leftValue = leftValue;
         this.operand = operand;
         this.rightValue = rightValue;
+    }
+
+    #logSetterErrors(badValue, classVar) {
+        console.error(`Invalid Value: ${badValue}\nPassed To: ${classVar}`);
     }
 
     get leftValue() {
@@ -17,15 +23,29 @@ class Calculator {
         // Default initialization value
         if (value === null) {
             this.#leftValue = null;
+            return;
         }
 
-        else if (typeof(value) !== 'number' || isNaN(value)) {
-            throw new Error(`Invalid value passed to leftValue: ${value}`);
+        if (typeof(value) !== 'number' || isNaN(value)) {
+            this.#logSetterErrors(value, '#leftValue');
             // call reset function
         }
 
         else {
             this.#leftValue = value;
+        }
+    }
+
+    get operand() {
+        return this.#operand;
+    }
+
+    set operand(symbol) {
+        if (symbol === null) {
+            this.#operand = null;
+        }
+        else if (!this.validOperands.includes(symbol)) {
+
         }
     }
 }
